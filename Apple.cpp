a08f29f12ed6dd500210dac96b138784ec26db16
Alokkagarwal18 <Alokagarwal1812@gmail.com>
2025-07-20 16:23:15 +0530

    code correct

--- Apple.cpp
+++ Apple.cpp
@@ -5,9 +5,9 @@ int main(){
     cout<<"Niteesh Kumar";
     cout<<"Alok Agarwal";
     cout<<"niteesh ram";
-    return 0;
     cout<<"me aa gya hu";
     cout<<"mei bhi aa raha hu";
     cout<<"code chal rha hai ";
     cout<<"mera bhi chal raha hai";
+    return 0;
 }
